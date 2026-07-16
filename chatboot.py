7d9b918b01fab5ba9b4ec1a647e2c56d82ddeb3f
benbelgacemmed <76822819+benbelgacemmed@users.noreply.github.com>
2026-07-17 00:39:19 +1200

    Update chatboot.py

--- chatboot.py
+++ chatboot.py
@@ -9,7 +9,20 @@ load_dotenv()
 api_key = os.getenv('GROQ_API_KEY') 
 
 st.title("Mohamed ChatBoot")
+import streamlit as st
 
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 #Creation of LLM's
 prompt_template = ChatPromptTemplate.from_messages(
     [
